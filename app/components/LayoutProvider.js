7778bbbf5482858ffd901948076f85f7315d9e5a
JiriDavid <jiridavidpromise@gmail.com>
2025-11-29 16:08:41 +0530

    Fix background color display on mobile devices

--- app/components/LayoutProvider.js
+++ app/components/LayoutProvider.js
@@ -23,7 +23,7 @@ export default function LayoutProvider({ children }) {
   return (
     <div className="relative min-h-screen">
       {/* Background pattern */}
-      <div className="absolute inset-0 bg-[url('/images/grid.svg')] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
+      <div className="absolute inset-0 bg-[url('/images/grid.svg')] bg-center opacity-10" />
       <div className="relative">
         <Navigation />
         <main>{children}</main>
